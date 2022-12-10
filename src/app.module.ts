231dbbe09563c08977b3a055733d2a8a362f32f2
mikenthiwa <mike.nthiwa@gmail.com>
2022-12-10 10:42:56 +0300

    Add npx mrm@2 lint-staged

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:Kenya2019@lifestyle.s6ogura.mongodb.net/?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
